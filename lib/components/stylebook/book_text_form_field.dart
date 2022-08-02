481b8be33fbcbf3704f0d51c81f4f129d25090b9
Retsuki <retu1118@gmail.com>
2022-08-03 08:39:20 +0900

    警告修正

--- lib/components/stylebook/book_text_form_field.dart
+++ lib/components/stylebook/book_text_form_field.dart
@@ -45,10 +45,7 @@ class _BookTextFormFieldState extends State<BookTextFormField> {
               const Gap(10),
               ElevatedButton(
                 onPressed: () {
-                  // Validate returns true if the form is valid, or false otherwise.
                   if (_formKey.currentState!.validate()) {
-                    // If the form is valid, display a snackbar. In the real world,
-                    // you'd often call a server or save the information in a database.
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(content: Text('Processing Data')),
                     );
