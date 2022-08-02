49cf77bdc2b7e0287c9f93eeb1048215f18e4190
Retsuki <retu1118@gmail.com>
2022-08-02 09:07:55 +0900

    themeアップデート

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class App extends ConsumerWidget {
     final router = ref.watch(routerProvider);
     return MaterialApp.router(
       title: 'Dating App',
-      theme: lightTheme,
+      theme: lightTheme(),
       routeInformationProvider: router.routeInformationProvider,
       routeInformationParser: router.routeInformationParser,
       routerDelegate: router.routerDelegate,
