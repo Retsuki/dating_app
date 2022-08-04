05cacf6e532b2f2f6591d2e5d4017302a511e8ba
Retsuki <retu1118@gmail.com>
2022-08-05 08:13:01 +0900

    ログイン画面追加

--- lib/router.dart
+++ lib/router.dart
@@ -2,13 +2,15 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
+import 'components/stylebook/book_back_button.dart';
 import 'components/stylebook/book_button.dart';
 import 'components/stylebook/book_text_form_field.dart';
 import 'screen/onboarding/onboarding_page.dart';
+import 'screen/sign_in/sign_in_page.dart';
 
 final routerProvider = Provider((ref) {
   return GoRouter(
-    initialLocation: '/${OnboardingPage.routeName}',
+    initialLocation: '/${SignInPage.routeName}',
     debugLogDiagnostics: kDebugMode,
     routes: [
       GoRoute(
@@ -22,9 +24,19 @@ final routerProvider = Provider((ref) {
         builder: (context, _) => const BookTextFormField(),
       ),
       GoRoute(
+        path: '/${BookBackButton.routeName}',
+        name: '/${BookBackButton.routeName}',
+        builder: (context, _) => const BookBackButton(),
+      ),
+      GoRoute(
         path: '/${OnboardingPage.routeName}',
         name: '/${OnboardingPage.routeName}',
         builder: (context, _) => const OnboardingPage(),
+      ),
+      GoRoute(
+        path: '/${SignInPage.routeName}',
+        name: '/${SignInPage.routeName}',
+        builder: (context, _) => const SignInPage(),
       ),
     ],
   );
