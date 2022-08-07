012bf838fb28e1dc50eae8e38bc9cf62ed6a43fd
Retsuki <retu1118@gmail.com>
2022-08-07 16:46:09 +0900

    性別画面追加

---
+++ lib/screen/setup/setup_gender/setup_gender_page.dart
@@ -0,0 +1,42 @@
+import 'package:dating_app/components/app_back_button_with_text.dart';
+import 'package:dating_app/components/app_button.dart';
+import 'package:dating_app/components/app_select_form_field.dart';
+import 'package:dating_app/l10n/l10n.dart';
+import 'package:flutter/material.dart';
+
+class SetupGenderPage extends StatelessWidget {
+  const SetupGenderPage({super.key});
+
+  static const routeName = 'setup-gender';
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = L10n.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: AppBackButtonWithText(text: l10n.gender),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+          child: Column(
+            children: [
+              AppSelectFormField(
+                labelText: l10n.gender,
+                shrinkWrap: true,
+                actionItems: [l10n.male, l10n.female, l10n.other],
+                onSelectedValue: (value) {},
+              ),
+              const Spacer(),
+              FilledButton(
+                text: l10n.goNext,
+                type: Type.round,
+                onPressed: () {},
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
