f86d6ac8b21772843d881bdebb94cee377d5ac0e
Retsuki <retu1118@gmail.com>
2022-08-25 08:35:19 +0900

    電話番号送信処理追加

--- lib/features/authentication/screens/phone_page.dart
+++ lib/features/authentication/screens/phone_page.dart
@@ -3,25 +3,29 @@ import 'package:dating_app/components/app_bottom_sheet.dart';
 import 'package:dating_app/components/app_button.dart';
 import 'package:dating_app/components/app_text_form_field.dart';
 import 'package:dating_app/components/un_focus_on_tap.dart';
+import 'package:dating_app/features/authentication/applications/phone_auth_state_notifier.dart';
 import 'package:dating_app/l10n/l10n.dart';
 import 'package:dating_app/utils/validators/phone/phone_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PhonePage extends StatelessWidget {
+class PhonePage extends ConsumerWidget {
   const PhonePage({super.key});
 
   static const routeName = 'phone';
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final l10n = L10n.of(context);
-    final formKey = GlobalKey<FormState>();
+
+    final phoneAuthNotifier =
+        ref.watch(phoneAuthStateNotifierProvider.notifier);
     return Scaffold(
       appBar: AppBackButton(title: l10n.phone),
       body: UnfocusOnTap(
         child: Form(
-          key: formKey,
+          key: phoneAuthNotifier.phonePageFormKey,
           child: SafeArea(
             child: Padding(
               padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
@@ -38,6 +42,7 @@ class PhonePage extends StatelessWidget {
                       l10n: l10n,
                       phoneNumber: phoneNumber,
                     ),
+                    onChanged: phoneAuthNotifier.updatePhoneNumber,
                   ),
                 ],
               ),
@@ -48,13 +53,7 @@ class PhonePage extends StatelessWidget {
       bottomSheet: AppBottomSheet(
         child: FilledButton(
           text: l10n.goNext,
-          onPressed: () {
-            if (formKey.currentState!.validate()) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('Processing Data')),
-              );
-            }
-          },
+          onPressed: phoneAuthNotifier.verifyPhoneNumber,
         ),
       ),
     );
