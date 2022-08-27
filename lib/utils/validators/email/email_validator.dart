e3dac0886f8768bdab25c57c967ace609c41ef31
Retsuki <retu1118@gmail.com>
2022-08-27 15:48:00 +0900

    emailモデル・バリデーション追加

---
+++ lib/utils/validators/email/email_validator.dart
@@ -0,0 +1,17 @@
+import 'package:dating_app/features/authentication/models/email/email.dart';
+import 'package:dating_app/l10n/l10n.dart';
+
+String? emailValidator({
+  required L10n l10n,
+  required String? email,
+}) {
+  final validation = Email.validate(email);
+  switch (validation) {
+    case EmailValidation.valid:
+      return null;
+    case EmailValidation.empty:
+      return l10n.goNext;
+    case EmailValidation.invalidFormat:
+      return l10n.goNext;
+  }
+}
