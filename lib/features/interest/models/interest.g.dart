e8d1691631dc6bdcf8929de83fd5d386d9f0ca5b
Retsuki <retu1118@gmail.com>
2022-08-30 07:39:54 +0900

    chore

--- lib/features/interest/models/interest.g.dart
+++ lib/features/interest/models/interest.g.dart
@@ -1,26 +1,28 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// ignore_for_file: type=lint, implicit_dynamic_parameter, implicit_dynamic_type, implicit_dynamic_method, strict_raw_type
+
 part of 'interest.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-_$_Interest _$$_InterestFromJson(Map<String, dynamic> json) => _$_Interest(
+_$_Interest _$$_InterestFromJson(Map json) => _$_Interest(
       createdAt: const DateTimeTimestampConverter()
-          .fromJson(json['createdAt'] as Object),
+          .fromJson(json['created_at'] as Object),
       updatedAt: const DateTimeTimestampConverter()
-          .fromJson(json['updatedAt'] as Object),
+          .fromJson(json['updated_at'] as Object),
       name: json['name'] as String,
-      isShow: json['isShow'] as bool,
+      isShow: json['is_show'] as bool,
     );
 
 Map<String, dynamic> _$$_InterestToJson(_$_Interest instance) =>
     <String, dynamic>{
-      'createdAt':
+      'created_at':
           const DateTimeTimestampConverter().toJson(instance.createdAt),
-      'updatedAt':
+      'updated_at':
           const DateTimeTimestampConverter().toJson(instance.updatedAt),
       'name': instance.name,
-      'isShow': instance.isShow,
+      'is_show': instance.isShow,
     };
