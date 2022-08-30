b541f11512416e8448c6ea2ccd9438946cc477c9
Retsuki <retu1118@gmail.com>
2022-08-30 09:11:46 +0900

    StringTimestampConverter修正

--- lib/utils/json_converters/string_timestamp_converter.dart
+++ lib/utils/json_converters/string_timestamp_converter.dart
@@ -1,17 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dating_app/constants/constant_datetime.dart';
+import 'package:dating_app/utils/extensions/extension_string.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-class StringTimestampConverter implements JsonConverter<String, Object> {
+class StringTimestampConverter implements JsonConverter<String?, Object?> {
   const StringTimestampConverter();
 
   @override
-  String fromJson(Object timestamp) {
-    return dateFormatFull.format((timestamp as Timestamp).toDate());
+  String? fromJson(Object? timestamp) {
+    return timestamp == null
+        ? null
+        : dateFormatFull.format((timestamp as Timestamp).toDate());
   }
 
   @override
-  Timestamp toJson(String datetimeString) {
-    return Timestamp.fromDate(dateFormatFull.parseStrict(datetimeString));
+  Timestamp? toJson(String? datetimeString) {
+    return datetimeString.isNullOrEmpty
+        ? null
+        : Timestamp.fromDate(dateFormatFull.parseStrict(datetimeString!));
   }
 }
