e1bbcb2c74ff3954336517a117c0baffbecf0b47
Retsuki <retu1118@gmail.com>
2022-08-31 08:48:02 +0900

    必要なもの追加

--- lib/features/user/screens/setup/setup_address_page.dart
+++ lib/features/user/screens/setup/setup_address_page.dart
@@ -3,36 +3,83 @@ import 'package:dating_app/components/app_bottom_sheet.dart';
 import 'package:dating_app/components/app_button.dart';
 import 'package:dating_app/components/app_text_form_field.dart';
 import 'package:dating_app/components/un_focus_on_tap.dart';
+import 'package:dating_app/features/user/applications/setup/setup_state_notifier.dart';
 import 'package:dating_app/features/user/screens/setup/setup_gender_page.dart';
 import 'package:dating_app/l10n/l10n.dart';
+import 'package:dating_app/utils/validators/setup/required_form.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 
-class SetupAddressPage extends StatelessWidget {
+class SetupAddressPage extends ConsumerWidget {
   const SetupAddressPage({super.key});
 
   static const routeName = 'setup-address';
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final l10n = L10n.of(context);
+    final setupNotifier = ref.watch(setupStateNotifierProvider.notifier);
+
     return Scaffold(
       appBar: AppBackButton(title: l10n.address),
       body: UnfocusOnTap(
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
-            child: Column(
-              children: [
-                AppTextFormField(labelText: l10n.prefecture),
-                const Gap(24),
-                AppTextFormField(labelText: l10n.city),
-                const Gap(24),
-                AppTextFormField(labelText: l10n.address1),
-                const Gap(24),
-                AppTextFormField(labelText: l10n.address2),
-              ],
+        child: Form(
+          key: setupNotifier.setupFormKeys[SetupFormType.address],
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+              child: Column(
+                children: [
+                  AppTextFormField(
+                    labelText: l10n.zipcode,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                      LengthLimitingTextInputFormatter(7),
+                    ],
+                    controller: setupNotifier.zipcodeTextController,
+                    validator: (value) => requiredFormValidator(
+                      value: value,
+                      message: l10n.somethingIsRequired(l10n.sei),
+                    ),
+                  ),
+                  const Gap(24),
+                  AppTextFormField(
+                    labelText: l10n.prefecture,
+                    controller: setupNotifier.prefectureTextController,
+                    validator: (value) => requiredFormValidator(
+                      value: value,
+                      message: l10n.somethingIsRequired(l10n.sei),
+                    ),
+                  ),
+                  const Gap(24),
+                  AppTextFormField(
+                    labelText: l10n.city,
+                    controller: setupNotifier.cityTextController,
+                    validator: (value) => requiredFormValidator(
+                      value: value,
+                      message: l10n.somethingIsRequired(l10n.sei),
+                    ),
+                  ),
+                  const Gap(24),
+                  AppTextFormField(
+                    labelText: l10n.address1,
+                    controller: setupNotifier.streetTextController,
+                    validator: (value) => requiredFormValidator(
+                      value: value,
+                      message: l10n.somethingIsRequired(l10n.sei),
+                    ),
+                  ),
+                  const Gap(24),
+                  AppTextFormField(
+                    labelText: l10n.address2,
+                    controller: setupNotifier.buildingTextController,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -40,8 +87,11 @@ class SetupAddressPage extends StatelessWidget {
       bottomSheet: AppBottomSheet(
         child: FilledButton(
           text: l10n.goNext,
-          onPressed: () {
-            context.goNamed(SetupGenderPage.routeName);
+          onPressed: () async {
+            if (await setupNotifier
+                .saveToFirestoreIfValid(SetupFormType.address)) {
+              context.goNamed(SetupGenderPage.routeName);
+            }
           },
         ),
       ),
