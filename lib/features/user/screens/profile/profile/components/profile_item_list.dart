a086861436e04c9decc74fc8625a44bb120370da
Retsuki <retu1118@gmail.com>
2022-09-04 08:28:52 +0900

    ログアウト機能追加

--- lib/features/user/screens/profile/profile/components/profile_item_list.dart
+++ lib/features/user/screens/profile/profile/components/profile_item_list.dart
@@ -1,15 +1,17 @@
 import 'package:collection/collection.dart';
+import 'package:dating_app/features/user/applications/profile/profile_provider.dart';
 import 'package:dating_app/features/user/models/profile/profile_item.dart';
 import 'package:dating_app/l10n/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ProfileItemList extends StatelessWidget {
+class ProfileItemList extends ConsumerWidget {
   const ProfileItemList({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final l10n = L10n.of(context);
     final theme = Theme.of(context);
     final textTheme = theme.textTheme;
@@ -18,7 +20,9 @@ class ProfileItemList extends StatelessWidget {
       children: ProfileItem.values.mapIndexed(
         (i, profileItem) {
           return InkWell(
-            onTap: () {},
+            onTap: () => ref
+                .read(profileProvider)
+                .func(context: context, profileItem: profileItem),
             child: Container(
               padding: const EdgeInsets.symmetric(vertical: 12),
               decoration: BoxDecoration(
