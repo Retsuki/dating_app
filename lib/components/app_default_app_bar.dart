045b78dd93d962f06342bba54e7ef9e650463218
Retsuki <retu1118@gmail.com>
2022-09-04 08:58:42 +0900

    アイコン・アクションを引数で渡すように

--- lib/components/app_default_app_bar.dart
+++ lib/components/app_default_app_bar.dart
@@ -1,28 +1,23 @@
 import 'package:dating_app/gen/assets.gen.dart';
-import 'package:dating_app/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 
 class AppDefaultAppBar extends StatelessWidget with PreferredSizeWidget {
   const AppDefaultAppBar({
     super.key,
+    required this.title,
+    this.actions,
   });
+
+  final String title;
+  final List<Widget>? actions;
 
   @override
   Widget build(BuildContext context) {
-    final l10n = L10n.of(context);
-    final theme = Theme.of(context);
-    final colorScheme = theme.colorScheme;
     return AppBar(
       leading: Assets.images.logo.app.image(scale: 4),
-      title: Text(l10n.profile, style: const TextStyle(color: Colors.black)),
+      title: Text(title, style: const TextStyle(color: Colors.black)),
       centerTitle: false,
-      actions: [
-        IconButton(
-          icon: const Icon(Icons.more_vert),
-          color: colorScheme.primary,
-          onPressed: () {},
-        )
-      ],
+      actions: actions,
     );
   }
 
