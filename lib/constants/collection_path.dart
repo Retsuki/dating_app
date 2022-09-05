fb355e5e5a3433a09252fa5f9a288c1c160d3736
Retsuki <retu1118@gmail.com>
2022-09-06 07:52:19 +0900

    collection path追加

--- lib/constants/collection_path.dart
+++ lib/constants/collection_path.dart
@@ -2,4 +2,6 @@ class ColName {
   static const user = 'user';
   static const privateUser = 'private_user';
   static const readOnlyUser = 'read_only_user';
+  static const chat = 'chat';
+  static const chatMessage = 'chat_message';
 }
