7993a67d15f8c905261c3e075740cffaab396676
Retsuki <retu1118@gmail.com>
2022-09-12 09:03:04 +0900

    AppBarAction追加

--- lib/components/app_default_app_bar.dart
+++ lib/components/app_default_app_bar.dart
@@ -1,5 +1,6 @@
 import 'package:dating_app/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class AppDefaultAppBar extends StatelessWidget with PreferredSizeWidget {
   const AppDefaultAppBar({
@@ -14,13 +15,43 @@ class AppDefaultAppBar extends StatelessWidget with PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      toolbarHeight: 50,
       leading: Assets.images.logo.app.image(scale: 4),
       title: Text(title, style: const TextStyle(color: Colors.black)),
       centerTitle: false,
-      actions: actions,
+      actions: actions == null ? [] : [...actions!, const Gap(16)],
     );
   }
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class AppBarAction extends StatelessWidget {
+  const AppBarAction({
+    super.key,
+    required this.onTap,
+    required this.icon,
+  });
+
+  final VoidCallback onTap;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return InkWell(
+      borderRadius: BorderRadius.circular(12),
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: colorScheme.primary.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Icon(icon, color: colorScheme.primary),
+      ),
+    );
+  }
 }
