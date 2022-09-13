e17673b9d207b68a1ca6f08e494fcf9efa2f814d
Retsuki <retu1118@gmail.com>
2022-09-13 09:39:35 +0900

    生年月日(string)を年齢(int)に変換する関数追加

--- lib/utils/date_formatter/date_to_string.dart
+++ lib/utils/date_formatter/date_to_string.dart
@@ -4,3 +4,16 @@ import 'package:dating_app/constants/constant_datetime.dart';
 String dateToTimeString(DateTime dateTime) {
   return dateFormatTime.format(dateTime);
 }
+
+/// 生年月日(string)を年齢(int)に変換する
+int birthdayStrToAge(String birthday) {
+  int calc(DateTime dateTime) {
+    return dateTime.year * 10000 + dateTime.month * 100 + dateTime.day;
+  }
+
+  final birthdayDate = dateFormatFull.parse(birthday);
+  final now = DateTime.now();
+  final age = (calc(now) - calc(birthdayDate)) / 10000;
+
+  return age.floor();
+}
