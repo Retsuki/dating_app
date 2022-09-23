0ad6d8afcd153ed36e0c144966825158ad3f023a
Retsuki <retu1118@gmail.com>
2022-09-24 07:00:58 +0900

    ホーム画面追加

---
+++ lib/features/home/screens/user_detail/components/user_profile_info.dart
@@ -0,0 +1,45 @@
+import 'package:dating_app/features/home/applications/user_detail/user_detail_provider.dart';
+import 'package:dating_app/l10n/l10n.dart';
+import 'package:dating_app/utils/date_formatter/date_to_string.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
+
+class UserProfileInfo extends ConsumerWidget {
+  const UserProfileInfo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final l10n = L10n.of(context);
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+    final user = ref.watch(userDetailFutureProvider).value;
+    if (user == null) {
+      return const SizedBox.shrink();
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            l10n.nickNameAndAge(
+              user.nickName,
+              birthdayStrToAge(user.birthday!),
+            ),
+            style: textTheme.displayLarge,
+          ),
+          const Gap(6),
+          Text(
+            'デザイナー',
+            style: textTheme.bodyLarge,
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/home/screens/user_detail/user_detail_page.dart
@@ -0,0 +1,58 @@
+import 'package:dating_app/components/app_back_button.dart';
+import 'package:dating_app/features/home/applications/user_detail/user_detail_provider.dart';
+import 'package:dating_app/features/home/screens/user_detail/components/user_profile_images.dart';
+import 'package:dating_app/features/home/screens/user_detail/components/user_profile_info.dart';
+import 'package:dating_app/features/user/models/user/user.dart';
+import 'package:dating_app/utils/extensions/extension_string.dart';
+
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class UserDetailPage extends HookConsumerWidget {
+  const UserDetailPage({
+    super.key,
+    required this.userId,
+  });
+
+  static const routeName = 'user-detail';
+  final String userId;
+
+  List<String> getUserImageList(User user) {
+    final imageList = [
+      user.mainImage,
+      user.subImage1,
+      user.subImage2,
+      user.subImage3
+    ];
+    return imageList.fold([], (previousValue, image) {
+      if (image.isNullOrEmpty) {
+        return previousValue;
+      }
+
+      previousValue.add(image!);
+      return previousValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(userDetailFutureProvider).value;
+    if (user == null) {
+      return const SizedBox.shrink();
+    }
+
+    return Scaffold(
+      appBar: AppBackButton(title: user.nickName),
+      body: SafeArea(
+        child: ListView(
+          children: const [
+            UserProfileImages(),
+            Gap(40),
+            UserProfileInfo(),
+          ],
+        ),
+      ),
+    );
+  }
+}
