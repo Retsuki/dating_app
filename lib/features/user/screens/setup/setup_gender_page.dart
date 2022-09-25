1ad2ccd6dd20e13f620aeec5498a9544b8682607
Retsuki <retu1118@gmail.com>
2022-09-26 07:03:02 +0900

    性別をsetupSelectに変更

--- lib/features/user/screens/setup/setup_gender_page.dart
+++ lib/features/user/screens/setup/setup_gender_page.dart
@@ -1,9 +1,7 @@
 import 'package:dating_app/components/app_back_button.dart';
-import 'package:dating_app/components/app_bottom_sheet.dart';
-import 'package:dating_app/components/app_button.dart';
-import 'package:dating_app/components/app_select_form_field.dart';
-import 'package:dating_app/components/un_focus_on_tap.dart';
 import 'package:dating_app/features/user/applications/setup/setup_state_notifier.dart';
+import 'package:dating_app/features/user/models/user/user.dart';
+import 'package:dating_app/features/user/screens/setup/components/setup_select.dart';
 import 'package:dating_app/features/user/screens/setup/setup_complete_page.dart';
 import 'package:dating_app/l10n/l10n.dart';
 import 'package:flutter/material.dart';
@@ -22,38 +20,19 @@ class SetupGenderPage extends ConsumerWidget {
 
     return Scaffold(
       appBar: AppBackButton(title: l10n.gender),
-      body: UnfocusOnTap(
-        child: Form(
-          key: setupNotifier.setupFormKeys[SetupFormType.gender],
-          child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
-              child: Column(
-                children: [
-                  AppSelectFormField(
-                    labelText: l10n.gender,
-                    shrinkWrap: true,
-                    actionItems: [l10n.male, l10n.female, l10n.other],
-                    controller: setupNotifier.genderTextController,
-                    onSelectedValue: (value) {
-                      setupNotifier.genderTextController.text = value;
-                    },
-                  ),
-                  const Spacer(),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-      bottomSheet: AppBottomSheet(
-        child: FilledButton(
-          text: l10n.goNext,
-          onPressed: () async {
-            if (await setupNotifier
-                .saveToFirestoreIfValid(SetupFormType.gender)) {
-              context.goNamed(SetupCompletePage.routeName);
-            }
+      body: SafeArea(
+        child: SetupSelect(
+          itemCount: UserGender.values.length,
+          itemBuilder: (context, index) {
+            final gender = UserGender.values[index];
+            return SetupSelectInkWell(
+              onTap: () async {
+                setupNotifier.genderTextController.text = gender.toStr();
+                await setupNotifier.saveToFirestore();
+                context.goNamed(SetupCompletePage.routeName);
+              },
+              text: gender.toStr(),
+            );
           },
         ),
       ),
