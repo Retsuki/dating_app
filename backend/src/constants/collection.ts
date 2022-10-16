abb16c37b8b7824f1193b3b377cb188151c9bcad
Retsuki <retu1118@gmail.com>
2022-10-17 01:52:57 +0900

    不要なもの削除

--- backend/src/constants/collection.ts
+++
@@ -1,8 +0,0 @@
-export class ColName {
-  static user = 'user'
-  static privateUser = 'private_user'
-  static privateUserWorry = 'private_user_worry'
-  static readonlyUser = 'readonly_user'
-
-  static worry = 'worry'
-}
