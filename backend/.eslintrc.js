4925ac9798821b71116f8108bcfb1c99fda6adec
Retsuki <retu1118@gmail.com>
2022-11-04 01:20:28 +0900

    eslintにルール追加

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -22,9 +22,43 @@ module.exports = {
     'node_modules',
     '.eslintrc.*',
   ],
-  plugins: ['@typescript-eslint', 'import'],
+  plugins: ['@typescript-eslint', 'import', 'unused-imports'],
   rules: {
     'import/no-unresolved': 0,
     'require-jsdoc': 0,
+    'valid-jsdoc': 0,
+
+    // 未使用の import文を削除
+    'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+
+    // importの順番
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        'newlines-between': 'always', // import groups の間 1行あける
+        alphabetize: { order: 'asc', caseInsensitive: true }, // 大文字小文字関係なくアルファベット順にしたい
+        pathGroupsExcludedImportTypes: ['builtin'],
+      },
+    ],
   },
 }
