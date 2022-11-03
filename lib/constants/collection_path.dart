6b86463f4f7a13e5932222f81d0d6aa316227ef2
Retsuki <retu1118@gmail.com>
2022-11-04 01:28:48 +0900

    コレクション名修正

--- lib/constants/collection_path.dart
+++ lib/constants/collection_path.dart
@@ -2,7 +2,7 @@ class ColName {
   static const user = 'user';
   static const privateUser = 'private_user';
   static const privateUserWorry = 'private_user_worry';
-  static const readOnlyUser = 'read_only_user';
+  static const readonlyUser = 'readonly_user';
   static const chat = 'chat';
   static const chatMessage = 'chat_message';
 
